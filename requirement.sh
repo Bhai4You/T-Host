6944e407cfa8e8245b49ade65b574ee4eb56ba71
Sutariya Parixit <32614362+Bhai4You@users.noreply.github.com>
2018-01-21 13:01:39 +0530

    Add files via upload

---
+++ requirement.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+#
+#	 <==(_127.0.0.1_)==>
+#
+# There's No Place Like (127.0.0.1)
+#
+# _____Do Not Copy Or Modify_____
+#
+#
+# Name 	:	127.0.0.1 
+# Code		:	Shell
+# Coded by	:	Sutariya Parixit
+# Sec.code	:	8h4i
+# Site		:	bhai4You
+# Date		:	21-1-18
+#
+#
+#
+clear
+apt update -y
+apt upgrade -y
+apt install figlet -y
+apt install toilet -y 
+apt install nano -y 
+apt install ruby -y
+echo
+echo
+figlet -f pagga Basic installation | lolcat
+echo
+echo
+gem install lolcat
+apt install php -y
+apt install openssl -y
+clear
+echo
+echo
+echo
+echo -e " \e[1m\e[33m\t 	Successful Installed Required Pkg..!!!"
+sleep 3
+clear
+echo
+echo
+echo -e "\e[36m\t\tNow You Can Run \e[32mbash 127.0.0.1.sh\e[0m"
+echo
